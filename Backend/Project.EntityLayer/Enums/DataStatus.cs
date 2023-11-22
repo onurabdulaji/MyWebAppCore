b287180aac63e6e83c05cde0a8913e77721b71dd
Onur Abdulaji <onur.abdulai@halkinsurance.com.mk>
2023-11-22 23:44:45 +0100

    EL Enum ve Interfaces eklendi

---
+++ Backend/Project.EntityLayer/Enums/DataStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.EntityLayer.Enums
+{
+    public enum DataStatus
+    {
+        Inserted = 1,
+
+        Deleted = 2,
+
+        Updated = 3,
+    }
+}
---
+++ Backend/Project.EntityLayer/Interfaces/IEntity.cs
@@ -0,0 +1,17 @@
+﻿using Project.EntityLayer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.EntityLayer.Interfaces
+{
+    public interface IEntity
+    {
+        public DateTime InsertedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public DataStatus Status { get; set; }
+    }
+}
