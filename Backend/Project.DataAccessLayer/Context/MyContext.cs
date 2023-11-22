668688e4de067b15f98ac67c7bc26354a33b0fa2
Onur Abdulaji <onur.abdulai@halkinsurance.com.mk>
2023-11-23 00:13:55 +0100

    DAL
    Context olusturuldu
    Konfigurasyonlar gecildi .
    Dbsetler gecildi.

---
+++ Backend/Project.DataAccessLayer/Context/MyContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project.DataAccessLayer.Configurations;
+using Project.EntityLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DataAccessLayer.Context
+{
+    public class MyContext : IdentityDbContext<AppUser, AppRole, int>
+    {
+        public MyContext(DbContextOptions<MyContext> opt) : base(opt)
+        {
+            //
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new AboutConfiguration());
+            builder.ApplyConfiguration(new AppRoleConfiguration());
+            builder.ApplyConfiguration(new AppUserConfiguration());
+            builder.ApplyConfiguration(new ContactConfiguration());
+            builder.ApplyConfiguration(new MyClientConfiguration());
+            builder.ApplyConfiguration(new ServiceConfiguration());
+            builder.ApplyConfiguration(new TestimonialConfiguration());
+            builder.ApplyConfiguration(new WorkConfiguration());
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<AppRole> AppRoles { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<MyClient> MyClients { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<Work> Works { get; set; }
+    }
+}
