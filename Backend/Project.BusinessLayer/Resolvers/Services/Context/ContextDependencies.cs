643d43e22457cbed74d7cc6f6293344c8a345bf4
Onur Abdulaji <onur.abdulai@halkinsurance.com.mk>
2023-11-23 01:13:52 +0100

    BAL
    Context Dependencies Eklendi

---
+++ Backend/Project.BusinessLayer/Resolvers/Services/Context/ContextDependencies.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Project.DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BusinessLayer.Resolvers.Services.Context
+{
+    public static class ContextDependencies
+    {
+        public static IServiceCollection ContextResolver(this IServiceCollection services)
+        {
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+            IConfiguration configuration = serviceProvider.GetService<IConfiguration>();
+            services.AddDbContextPool<MyContext>(opt =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("MyConnection"))
+                .UseLazyLoadingProxies();
+            });
+
+            return services;
+        }
+    }
+}
