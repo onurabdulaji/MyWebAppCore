d0b4a51adbd89a670445a45628198b4fae57b3fb
Onur Abdulaji <onur.abdulai@halkinsurance.com.mk>
2023-11-23 01:47:27 +0100

    BAL
    Mapper Resolver Eklendi

---
+++ Backend/Project.BusinessLayer/Resolvers/Services/AutoMapper/AutoMapperDependencies.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BusinessLayer.Resolvers.Services.AutoMapper
+{
+    public static class AutoMapperDependencies
+    {
+        public static IServiceCollection MapperResolver(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(AutoMapperDependencies));
+            return services;
+        }
+    }
+}
