2d76ae2e4fe63dafb2803299c33e5497f071ffc3
Onur Abdulaji <onur.abdulai@halkinsurance.com.mk>
2023-11-23 01:15:36 +0100

    BAL
    Cors Eklendi fakat servisleri yazilmadi .
    
    ! Yazilmasi Gerek

---
+++ Backend/Project.BusinessLayer/Resolvers/Configurations/Cors/CorsDependencies.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BusinessLayer.Resolvers.Configurations.Cors
+{
+    public static class CorsDependencies
+    {
+        public static IConfiguration CorsResolver(this IConfiguration configuration)
+        {
+            return configuration;
+        }
+    }
+}
