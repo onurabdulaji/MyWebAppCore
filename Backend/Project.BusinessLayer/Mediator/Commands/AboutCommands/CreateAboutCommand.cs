a8fec825dd73e62e7dcaf8ce9c2b71a64202bf9d
Onur Abdulaji <onur.abdulai@halkinsurance.com.mk>
2023-11-24 16:15:28 +0100

    #Primary CTOR

--- Backend/Project.BusinessLayer/Mediator/Commands/AboutCommands/CreateAboutCommand.cs
+++ Backend/Project.BusinessLayer/Mediator/Commands/AboutCommands/CreateAboutCommand.cs
@@ -3,19 +3,11 @@ using Project.DTOLayer.ApiDTO.AboutDTO;
 
 namespace Project.BusinessLayer.Mediator.Commands.AboutCommands
 {
-    public class CreateAboutCommand : IRequest<AboutDetails>
+    public class CreateAboutCommand(string AboutTitle, string AboutDescriptionOne, string AboutDescriptionTwo, bool AboutStatus) : IRequest<AboutDetails>
     {
-        public string AboutTitle { get; set; }
-        public string AboutDescriptionOne { get; set; }
-        public string AboutDescriptionTwo { get; set; }
-        public bool AboutStatus { get; set; }
-
-        public CreateAboutCommand(string AboutTitle, string AboutDescriptionOne, string AboutDescriptionTwo, bool AboutStatus)
-        {
-            this.AboutTitle = AboutTitle;
-            this.AboutDescriptionOne = AboutDescriptionOne;
-            this.AboutDescriptionTwo = AboutDescriptionTwo;
-            this.AboutStatus = AboutStatus;
-        }
+        public string AboutTitle { get; set; } = AboutTitle;
+        public string AboutDescriptionOne { get; set; } = AboutDescriptionOne;
+        public string AboutDescriptionTwo { get; set; } = AboutDescriptionTwo;
+        public bool AboutStatus { get; set; } = AboutStatus;
     }
 }
