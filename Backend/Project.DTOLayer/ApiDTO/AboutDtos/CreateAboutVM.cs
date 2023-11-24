b3f149870ebb18cd152b915764188303075800e3
Onur Abdulaji <onur.abdulai@halkinsurance.com.mk>
2023-11-24 10:57:56 +0100

    MediatR
    Butun MediatR islemleri degistirildi ,
    
    MediatR islemleri Entity yerine DTO kullanilmaya baslandi.

---
+++ Backend/Project.DTOLayer/ApiDTO/AboutDtos/CreateAboutVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DTOLayer.ApiDTO.AboutDtos
+{
+    public class CreateAboutVM
+    {
+        public string AboutTitle { get; set; }
+        public string AboutDescriptionOne { get; set; }
+    }
+}
---
+++ Backend/Project.DTOLayer/ApiDTO/AboutDtos/GetAboutDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DTOLayer.ApiDTO.AboutDtos
+{
+    public class GetAboutDto
+    {
+        public int AboutID { get; set; }
+        public string? AboutTitle { get; set; }
+        public string? AboutDescriptionOne { get; set; }
+    }
+}
